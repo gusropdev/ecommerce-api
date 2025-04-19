22c9ea96d913d21770b910a60ab381f09aaa42dd
gusropdev <gusropdev@hotmail.com>
2025-04-19 13:49:16 -0300

    feat(domain): adiciona entidades iniciais ao domínio

---
+++ RO.DevTest.Domain/Entities/OrderItem.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace RO.DevTest.Domain.Entities;
+
+public class OrderItem
+{
+    public Guid Id { get; set; }
+    
+    public Guid OrderId { get; set; }
+    public Order Order { get; set; } = null!;
+
+    public Guid ProductId { get; set; }
+    public Product Product { get; set; } = null!;
+
+    public int Quantity { get; set; }
+    public decimal UnitPrice { get; set; }
+
+    public decimal Subtotal => Quantity * UnitPrice;
+}
