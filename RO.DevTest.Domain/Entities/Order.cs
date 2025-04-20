790f42d3e9036c7e1ad91496c1908f9e7ef6a0b6
gusropdev <gusropdev@hotmail.com>
2025-04-20 11:13:06 -0300

    fix(domain): arruma FK  customerId de order

--- RO.DevTest.Domain/Entities/Order.cs
+++ RO.DevTest.Domain/Entities/Order.cs
@@ -3,8 +3,8 @@ namespace RO.DevTest.Domain.Entities;
 public class Order
 {
     public Guid Id { get; set; }
-    
-    public Guid CustomerId { get; set; }
+
+    public string CustomerId { get; set; } = null!;
     public Customer Customer { get; set; } = null!;
     
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
