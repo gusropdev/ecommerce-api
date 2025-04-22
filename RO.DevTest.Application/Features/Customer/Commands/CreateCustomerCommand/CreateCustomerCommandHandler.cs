b132bd6dd152e9247ee157a6b39df1427040dc2f
gusropdev <gusropdev@hotmail.com>
2025-04-22 18:40:41 -0300

    remove(application): remove códigos desnecessários em CreateCustommerCommand

--- RO.DevTest.Application/Features/Customer/Commands/CreateCustomerCommand/CreateCustomerCommandHandler.cs
+++ RO.DevTest.Application/Features/Customer/Commands/CreateCustomerCommand/CreateCustomerCommandHandler.cs
@@ -1,6 +1,4 @@
-using FluentValidation.Results;
 using MediatR;
-using Microsoft.AspNetCore.Identity;
 using RO.DevTest.Application.Contracts.Infrastructure;
 using RO.DevTest.Application.Contracts.Persistance.Repositories;
 using RO.DevTest.Domain.Enums;
@@ -14,9 +12,6 @@ public class CreateCustomerCommandHandler(
     : IRequestHandler<CreateCustomerCommandRequest, CreateCustomerResult>
 
 {
-    private readonly IIdentityAbstractor _identityAbstractor = identityAbstractor;
-    private readonly ICustomerRepository _customerRepository = customerRepository;
-
     public async Task<CreateCustomerResult> Handle(CreateCustomerCommandRequest request, CancellationToken cancellationToken)
     {
         var validator = new CreateCustomerCommandValidator();
@@ -26,11 +21,11 @@ public class CreateCustomerCommandHandler(
             throw new BadRequestException(validationResult);
         
         var user = request.AssignToUser();
-        var creationResult = await _identityAbstractor.CreateUserAsync(user, request.Password);
+        var creationResult = await identityAbstractor.CreateUserAsync(user, request.Password);
         if (!creationResult.Succeeded)
             throw new BadRequestException(creationResult);
         
-        var roleResult = await _identityAbstractor.AddToRoleAsync(user, UserRoles.Customer);
+        var roleResult = await identityAbstractor.AddToRoleAsync(user, UserRoles.Customer);
         if (!roleResult.Succeeded)
             throw new BadRequestException(roleResult);
         
@@ -40,7 +35,7 @@ public class CreateCustomerCommandHandler(
             User = user
         };
 
-        await _customerRepository.CreateAsync(customer, cancellationToken);
+        await customerRepository.CreateAsync(customer, cancellationToken);
 
         return new CreateCustomerResult(customer);
     }
