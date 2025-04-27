2452b8104d50938e299bab0c89b0401a79ba85ef
gusropdev <gusropdev@hotmail.com>
2025-04-27 18:36:55 -0300

    refactor: altera os métodos do repositório para não retornarem Void e funcionarem assincronamente de maneira correta
    Os métodos `Update` e `Delete` em `IBaseRepository` e `BaseRepository` foram atualizados para usar `Task` assíncrono, melhorando a escalabilidade e o desempenho. A propriedade `IsActive` na entidade `Product` também foi ajustada para maior clareza e consistência.

--- RO.DevTest.Domain/Entities/Product.cs
+++ RO.DevTest.Domain/Entities/Product.cs
@@ -8,7 +8,6 @@ public class Product : BaseEntity
     public string Description { get; set; } = string.Empty;
     public decimal Price { get; set; }
     public int StockQuantity { get; set; }
-    public bool IsActive { get; set; } = true;
-
+    public bool IsActive { get; set; }
     public List<OrderItem> OrderItems { get; set; } = [];
 }
